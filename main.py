0da3db1ce335edd805031827edebbcf12c434d6e
Shane <shanelibera7@gmail.com>
2026-09-16 22:08:01 +0100

    Code plan laid out

---
+++ main.py
@@ -0,0 +1,56 @@
+#Story Template decision for User 
+
+
+def story_template(choice):
+    choice = input("Choose your story Template:   " \
+"A) Superhero Story " \
+"B) Football Story " \
+"C) Space Adventure " \
+"D) Haunted House ")
+    #Return Selected template 
+    return chosen_template 
+
+
+
+
+#Validate input 
+def validate_input():
+    #Ask fopr input 
+    #Validate
+    #Return clean word 
+    return word 
+
+
+
+
+
+
+
+
+
+#Collect words 
+def collect_words(chosen_template):
+    # Look at what the chosen template needs
+    # Ask for each required word
+    # Return the words
+    return words
+
+
+
+#build Story  
+def build_story(chosen_template, words):
+    # Insert the words into the template
+    # Return the completed story
+    return story
+
+
+
+#Format Story 
+def format_story(story):
+    # Add title, line breaks, capitalisation, etc.
+    # Return final formatted story
+    return formatted_story
+
+
+
+#Main func 
