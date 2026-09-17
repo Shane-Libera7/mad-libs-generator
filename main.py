940d3e051ede2ea8f91511fdb69d88641408db43
Shane <shanelibera7@gmail.com>
2026-09-17 16:23:28 +0100

    2 of the 5 functions established

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 # Story template choice – function should return variable called chosen_template, 
 # string of the template chosen which will be referred to the rest of the code. 
 # To dictate sentence formation and word input
+def story_template():
+    
+    while True:
+        choice = input("Enter the lowercase letter of the Story Template you wish to use:  " \
+        "A) Superhero Story " \
+        "B) Space Adventure " \
+        "C) Haunted House ")
+        if choice == "a":
+            chosen_template = "Superhero Story"
+            break
+        elif choice == "b":
+            chosen_template = "Space Adventure"
+            break
+        elif choice == "c":
+            chosen_template = "Haunted House"
+            break
+        else: 
+            print("Invalid input please try again")
+            continue
+    
+    print(f"You have chosen {chosen_template}")
+    return chosen_template
+
+   
+        
 
 
 
+# Input Validation () - function will validate each input returns word -- Strip whitespace, checks its not empty and contains acceptable characters 
+def input_validation(prompt):
+   
+    
+    while True:
+       user_input = input(prompt)
+       word = str(user_input).strip()
+       if word == "":
+           print("Invalid Input. Enter input necessary")
+           continue
+       elif all(character.isalpha() or character  in " -" for character in word):
+           print("Input Accepted")
+           break
+       else:
+           print("Invalid input. Enter input necessary")
+           continue
+           
+           
+    return word 
+    
 
-# Input Validation () - function will validate each input returns word 
 
 
 
 
 # Input based on request (chosen template) – function will use input validation to ask each kind of word in order needed for chosen template 
 # return array of words needed in order of the story 
+def 
 
 
 
@@ -30,9 +75,14 @@
 
 
 #Main -- Format all functions together in correct order 
+def main():
+    story_template()
+    return
 
 
 
+
+main()
 
 
 
